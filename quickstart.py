deae4214db85a63b00c5fbf2a701cfebeff7fd39
Frank Palma <frankpalma12@gmail.com>
2018-12-13 20:42:01 -0500

    Added the function that lists the messages by matching each query which then returns the ID of all the messages under the query

--- quickstart.py
+++ quickstart.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+from apiclient import errors
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
@@ -22,6 +23,7 @@ def main():
 
     # Call the Gmail API
     results = service.users().labels().list(userId='me').execute()
+    # Gmails Labels
     labels = results.get('labels', [])
 
     if not labels:
@@ -30,6 +32,44 @@ def main():
         print('Labels:')
         for label in labels:
             print(label['name'])
+
+    #Calling List of Messages with the query 'Google'
+    print(ListMessagesMatchingQuery(service, 'me','Google'))
+
+
+def ListMessagesMatchingQuery(service, user_id, query):
+  """List all Messages of the user's mailbox matching the query.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    query: String used to filter messages returned.
+    Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+  Returns:
+    List of Messages that match the criteria of the query. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate ID to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               q=query).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id, q=query,
+                                         pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError, error:
+    print('An error occurred: %s' % error)
+
+
 
 if __name__ == '__main__':
     main()
