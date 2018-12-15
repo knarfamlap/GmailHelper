a1ca1381373a2257cc4e5ff251901979657333d1
Frank Palma <frankpalma12@gmail.com>
2018-12-15 00:24:12 -0500

    Troubleshooting how to send data to template

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import render_template
+from quickstart import messageSnippets
 
 app = Flask(__name__)
 
+print(len(messageSnippets))
+
 @app.route('/')
-def hello_world():
-    return 'Hello'
+def main_page(message = messageSnippets):
+    #rendering messages snippets
+    return render_template('messages.html', message=message)
